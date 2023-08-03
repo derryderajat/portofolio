f349e759ca76a12dd0145f6338d96633fc466c67
derryderajat <derryd.derajat@gmail.com>
2023-08-04 06:20:33 +0700

    Complete about me

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,37 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+const Skill = ({ name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      animate={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+      viewport={{ once: true }}
+    >
+      {name}
+    </motion.div>
+  );
+};
 function Skills() {
   return (
     <>
       <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
-      <div className="w-full h-screen relative flex items-center justify-center"></div>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <div className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer absolute">
+          WEB
+        </div>
+        <Skill name={"HTML"} x="10vw" y="2vw" />
+        <Skill name={"CSS"} x="-5vw" y="-2vw" />
+        <Skill name={"Javascript"} x="-15vw" y="0vw" />
+        <Skill name={"NextJS"} x="0vw" y="12vw" />
+        <Skill name={"GIT"} x="0vw" y="-15vw" />
+        <Skill name={"Python"} x="30vw" y="-5vw" />
+        <Skill name={"Figma"} x="-15vw" y="-12vw" />
+        <Skill name={"Flutter"} x="18vw" y="18vw" />
+        <Skill name={"Cypress"} x="-25vw" y="18vw" />
+      </div>
     </>
   );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,8 @@ import profilePic from "../../public/images/profile/developer-pic-2.png";
 import Image from "next/image";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -108,6 +110,8 @@ function about() {
             </div>
           </div>
           <Skills />
+          <Experience />
+          <Education />
         </Layout>
       </main>
     </>
