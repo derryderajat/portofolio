cde4760db23efa9e8a7ca0d3815ac6a98d390345
derryderajat <derryd.derajat@gmail.com>
2023-08-03 17:49:18 +0700

    Update Foto profile

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,11 @@ export default function Home() {
         <Layout className="pt-0">
           <div className="flex items-center justify-betweem w-full">
             <div className="w-1/2">
-              <div className="w-2/5">
-                <Image
-                  src={profilePic}
-                  alt="Derry"
-                  className="w-full h-auto rounded-md"
-                />
-              </div>
+              <Image
+                src={profilePic}
+                alt="Derry"
+                className="w-full h-auto rounded-md"
+              />
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText
