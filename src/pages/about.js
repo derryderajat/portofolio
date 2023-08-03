9688398b4735fd6271acd2a8c969845b57799e0b
derryderajat <derryd.derajat@gmail.com>
2023-08-03 19:32:48 +0700

    Menambahkan laman About

---
+++ src/pages/about.js
@@ -0,0 +1,116 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-2.png";
+import Image from "next/image";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    // first
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+function about() {
+  return (
+    <>
+      <Head>
+        <title>Derry Derajat | About Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText text={"Passion Fuels Purpose!"} className="mb-16" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi there, I'm Derry Derajat, a seasoned Fullstack Developer and
+                Software QA Specialist with a passion for creating exceptional
+                digital solutions that combine both functionality and quality.
+                With 2 years of experience in the industry, I have honed my
+                skills in web development, Android app development, and
+                comprehensive testing practices.
+              </p>
+              <p className="my-4 font-medium">
+                As a Fullstack Developer, I love tackling challenges and finding
+                innovative ways to bring my clients' visions to life. I firmly
+                believe that design and functionality go hand in hand, and I
+                strive to create user-centered digital experiences that are not
+                only visually appealing but also intuitive and enjoyable to use.
+              </p>
+              <p className="font-medium">
+                When it comes to Software Quality Assurance, my dedication to
+                detail and thoroughness sets me apart. I understand the
+                importance of delivering a flawless product, and I am committed
+                to ensuring that every aspect of the software is rigorously
+                tested and meets the highest standards of excellence.
+              </p>
+              <p className="my-4 font-medium">
+                Whether I'm coding a website, crafting an Android app, or
+                conducting comprehensive testing, my primary focus is on solving
+                problems and exceeding expectations. I approach every project
+                with a commitment to excellence, professionalism, and a
+                user-centered mindset.
+              </p>
+              <p className="font-medium">
+                I'm excited about the opportunity to contribute my skills and
+                passion to your next project. Let's collaborate and create
+                outstanding digital experiences together!
+              </p>
+            </div>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8">
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Derry Derajat"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+            <div className="col-span-2 flex flex-col items-end justify-evenly ">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={10} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Projects
+                </h2>
+              </div>
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={2} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  Years of Experence
+                </h2>
+              </div>
+            </div>
+          </div>
+          <Skills />
+        </Layout>
+      </main>
+    </>
+  );
+}
+
+export default about;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
-import profilePic from "../../public/images/profile/developer-pic-1.png";
 import Image from "next/image";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
+import HireMe from "@/components/HireMe";
+import profilePic from "../../public/images/profile/developer-pic-1.png";
+import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
 export default function Home() {
   return (
     <>
@@ -55,6 +57,14 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className="absolute right-8 bottom-8 inline-block w-24">
+          <Image
+            src={lightBulb}
+            alt="Derry Derajat"
+            className="w-full h-auto"
+          />
+        </div>
       </main>
     </>
   );
